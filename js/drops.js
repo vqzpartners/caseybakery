d30e3218705f27ef31e1b836b31c5397ddd6d4fc
u09093023 <189559174+u09093023@users.noreply.github.com>
2025-04-05 00:07:32 -0500

    Update drops.js

--- js/drops.js
+++ js/drops.js
@@ -7,7 +7,7 @@ const bakeryDrops = [
         status: "available", // "available" or "coming-soon"
         image: "img/casey/maracuya.webp", // Will be overridden with mystery image if status is "coming-soon"
         url: "https://cuanto.app/casey/w/6d97e2", // Only used if status is "available"
-        releaseDate: "2025-04-05T00:00:00" // Only used if status is "coming-soon" - Format: "YYYY-MM-DDThh:mm:ss"
+        releaseDate: null // Only used if status is "coming-soon" - Format: "YYYY-MM-DDThh:mm:ss"
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ const bakeryDrops = [
         status: "available",
         image: "img/casey/nutella-cake.jpg",
         url: "https://cuanto.app/casey/w/4164aa", //
-        releaseDate: "2025-04-05T00:00:00"
+        releaseDate: null
     },
     
     // Coming soon drops
@@ -25,7 +25,7 @@ const bakeryDrops = [
         status: "available",
         image: "img/casey/carrot.jpg",
         url: "https://cuanto.app/casey/w/3cf3e9",
-        releaseDate: "2025-04-05T00:00:00"
+        releaseDate: null
     },
     
     // More drops - all without timers
@@ -35,6 +35,6 @@ const bakeryDrops = [
         status: "available",
         image: "img/casey/pistacho.jpg", 
         url: "https://cuanto.app/casey/p/c21a35",
-        releaseDate: "2025-04-05T00:00:00"
+        releaseDate: null
     }
 ];
